248d705ac54e25a819aae645fcb5a5577ad0cccc
mirandaunwin <miranda.unwin@gmail.com>
2018-11-15 16:23:02 +0100

    5.1 rerouting and changing root

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  
-  root 'simple_pages#landing_page'
+  get 'simple_pages/landing_page'
+
+  root 'products#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
