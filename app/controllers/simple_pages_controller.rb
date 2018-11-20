2c06a61c5b1dbccf34eacb2b5273688c73ac6cc1
mirandaunwin <miranda.unwin@gmail.com>
2018-11-20 17:57:39 +0100

    5.3 adding email contact form

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,7 +1,19 @@
 class SimplePagesController < ApplicationController
+
   def index
   end
+
   def landing_page
     @products = Product.limit(3)
+  end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+      to: 'miranda.unwin@gmail.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
 
+  post 'simple_pages/thank_you'
+
   root 'simple_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
