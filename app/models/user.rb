408805a213543918d9da8848c8361398c0cb9ec3
mirandaunwin <miranda.unwin@gmail.com>
2018-11-26 18:36:58 +0100

    5.7 creating comment model & scopes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :comments
+  has_many :orders
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :orders
