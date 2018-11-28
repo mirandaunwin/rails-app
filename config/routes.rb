ac5e3fa9324bc33ca3d21c002e86b1872205b234
mirandaunwin <miranda.unwin@gmail.com>
2018-11-28 15:02:13 +0100

    5.8 styling reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
 
-  resources :users
-
   resources :products do
     resources :comments
   end
+
+  resources :users
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
