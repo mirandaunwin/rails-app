8c256bf114f84ca2e7f3efc41aca961793ba3721
mirandaunwin <miranda.unwin@gmail.com>
2018-12-13 10:57:49 +0100

    6.13 adding ActionCable

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
 
   root 'simple_pages#index'
 
+  mount ActionCable.server => '/cable'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
